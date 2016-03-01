0a0941dd69dadb71bf832bd54b744aa0152f5e00
Vibhav Pant <vibhavp@gmail.com>
2016-03-01 19:54:21 +0530

    Set all variables

--- env.sh
+++ env.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-sed -i 's|username|'${DB_USERNAME}$'|g' config.ini
-sed -i 's|password|'${DB_PASSWORD}$'|g' config.ini
-sed -i 's|127.0.0.1|'${DB_URL}$'|g' config.ini
-sed -i 's|database|'${DB_NAME}$'|g' config.ini
-sed -i 's|icesecret|'${DB_NAME}$'|g' config.ini
+sed -i 's|username|'${DB_USERNAME}$'|g' /mumble-authenticator/config.ini
+sed -i 's|password|'${DB_PASSWORD}$'|g' /mumble-authenticator/config.ini
+sed -i 's|dbhost|'${DB_URL}$'|g' /mumble-authenticator/config.ini
+sed -i 's|database|'${DB_NAME}$'|g' /mumble-authenticator/config.ini
+sed -i 's|icesecret|'${ICE_SECRET}$'|g' /mumble-authenticator/config.ini
+sed -i 's|mumblehost|'${ICE_HOST}$'|g' /mumble-authenticator/config.ini
+sed -i 's|DEBUG|'${DEBUG}$'|g' /mumble-authenticator/config.ini
